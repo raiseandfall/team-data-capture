734e9c05cabfae985414ea93fdcff4f3c1a61c77
Thibaut Delille <thibaut@jvst.us>
2014-03-17 14:58:47 -0700

    remove comment in sockets

--- server/sockets.js
+++ server/sockets.js
@@ -45,37 +45,3 @@ var Socket = function() {
 };
 
 exports.Socket = Socket;
-/*
-module.exports = function(app) {
-    wss = new WebSocketServer( { server: app } );
-
-    wss.on('connection', function (ws) {
-      var _clientId = clientId++;
-
-      console.log('Client #%d connected !', _clientId, ws._socket.remoteAddress);
-
-      // If client already exists
-      //var cli = new Client( _clientId, ws._socket.remoteAddress );
-
-      //if (cli === true) {
-        // On message
-        ws.on('message', function (data, flags) {
-          console.log('Client #%d : %s', _clientId, data);
-        });
-
-        // Client disconnected
-        ws.on('close', function () {
-          console.log('Client #%d disconnected', _clientId);
-
-          // Unbind events
-
-          //cli.disconnect( _clientId );
-        });
-
-        // Client error
-        ws.on('error', function (e) {
-          console.log('Client #%d error: %s', e.message);
-        });
-      //}
-    });
-};*/
