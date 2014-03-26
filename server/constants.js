72a06a2fdb5a2334037116d9a8b50c45f0e633fb
Thibaut Delille <thibaut@jvst.us>
2014-03-25 17:13:24 -0700

    add readme

---
+++ server/constants.js
@@ -0,0 +1,24 @@
+
+/**
+ *  main():  Main code.
+ */
+exports.APP = {
+	TYPE: {
+		HELLO: 'hello',
+		AUTH: 'auth',
+		WELCOME: 'welcome',
+		NEW_USER: 'newuser',
+		CLOSE_USER: 'closeuser'
+	},
+	CLIENT: {
+		APP: 'app',
+		WEB: 'web'
+	},
+	ACTION:{
+		MOUSE_MOVE: 'mousemove',
+		CLICK: 'click',
+		KEY_DOWN: 'keydown',
+		MOUSE_WHEEL: 'mousewheel',
+		WORD:	'word'
+	}
+} 
