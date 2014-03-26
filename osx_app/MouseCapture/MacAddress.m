49959d12a753d7492306a8634b013e0d1245b7df
raiseandfall <matthew@jvst.us>
2014-03-25 18:03:46 -0700

    Added OSX Notifications

--- osx_app/MouseCapture/MacAddress.m
+++ osx_app/MouseCapture/MacAddress.m
@@ -1,3 +1,11 @@
+//
+//  MacAddress.m
+//  Team Data Capture
+//
+//  Created by Matthieu COLLE on 2/22/14.
+//  Copyright (c) 2014 Matthieu COLLE. All rights reserved.
+//
+
 #import "MacAddress.h"
 #include <sys/socket.h>
 #include <sys/sysctl.h>
